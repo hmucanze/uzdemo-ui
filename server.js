0d73106d373e58fe7b54b2f00cde72e16fba7164
HerminioMucanze19 <herminiomucanze@gmail.com>
2019-09-09 23:36:55 +0200

    adicionando meta tag CSP

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req, resp){
-  resp.sendFile(__dirname + 'dist/index.html');
+app.get('/*', function(req, res) {
+  res.sendFile(__dirname + 'dist/index.html');
 });
 
 app.listen(process.env.PORT || 4200);
